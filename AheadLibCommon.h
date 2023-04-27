85c12a35d6fd1dc5b6ac0b03a25293ae60b1ffbe
LOVEXIXI <740505846@QQ.COM>
2023-04-27 14:48:54 +0800

    修改版本号

--- AheadLibCommon.h
+++ AheadLibCommon.h
@@ -11,7 +11,7 @@
 #define STR_GITHUB_ADDRESS		TEXT("Github : https://github.com/i1tao/AheadLibEx")
 #define STR_BLOG				TEXT("Website : https://www.cnblogs.com/0xc5/")
 #define STR_51ASM				TEXT("        : https://www.51asm.com")
-#define STR_BUILD_VERSION		TEXT("1.0")
+#define STR_BUILD_VERSION		TEXT("1.1")
 #define STR_BUILD_DATE			TEXT(__DATE__)
 #define STR_BUILD_TIME			TEXT(__TIME__)
 #define STR_BUILD_DATE_TIME		TEXT(__DATE__) TEXT(" ") TEXT(__TIME__)
