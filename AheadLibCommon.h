aeaf5d7e3ae69978bb013f4ac036ea5e06762737
i1tao <lumosmagicb00m@gmail.com>
2023-10-10 03:36:02 +0800

    style(Ver):upgrade build version

--- AheadLibCommon.h
+++ AheadLibCommon.h
@@ -11,11 +11,11 @@
 #define STR_GITHUB_ADDRESS		TEXT("Github : https://github.com/i1tao/AheadLibEx")
 #define STR_BLOG				TEXT("Website : https://www.cnblogs.com/0xc5/")
 #define STR_51ASM				TEXT("        : https://www.51asm.com")
-#define STR_BUILD_VERSION		TEXT("1.2")
+#define STR_BUILD_VERSION		TEXT("1.3")
 #define STR_BUILD_DATE			TEXT(__DATE__)
 #define STR_BUILD_TIME			TEXT(__TIME__)
 #define STR_BUILD_DATE_TIME		TEXT(__DATE__) TEXT(" ") TEXT(__TIME__)
-#define STR_PROC_TITLE          TEXT("AheadLibEx Ver:") STR_BUILD_VERSION TEXT(" Build:") STR_BUILD_DATE
+#define STR_PROC_TITLE          TEXT("AheadLibEx Ver:") STR_BUILD_VERSION TEXT(" Build: ") STR_BUILD_DATE
 #define STR_COPYRIGHT			TEXT("Copyright (C) 2023 ") STR_AUTHOR_NAME TEXT(" . All rights reserved.")
 /***********************************************************************************************/
 
